10151bc7f6986346d0fcae4cd6af9cc56c4ef57e
kedyxa <kedyshkolex@gmail.com>
2022-04-04 15:02:13 +0300

    add lessons 1

---
+++ main.py
@@ -0,0 +1,9 @@
+# #lessons_1
+# import random
+# def mass_sum():
+#     mass = [random.randint(0, 10) for i in range(10)]
+#     print(mass)
+#     return print(sum(mass))
+# mass_sum()
+
+#lessons_2

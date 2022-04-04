ef88257015591d6461428a91cf515881f236a896
kedyxa <kedyshkolex@gmail.com>
2022-04-04 15:16:45 +0300

    added task 2 from the presentation of the function

--- main.py
+++ main.py
@@ -7,3 +7,10 @@
 # mass_sum()
 
 #lessons_2
+
+# def is_year_leap(year):
+#     return year % 4 == 0 and  year % 100 != 0 or  year % 400 ==0
+#
+#
+# while True:
+#     print(is_year_leap(int(input("введите год: "))))
